ae15e2980d6074d6ebb1abcb610d329c7c22be43
Jaison Zachariah <qburst.jaison@daimler.com>
2022-02-14 13:27:24 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Jaison Zachariah on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
